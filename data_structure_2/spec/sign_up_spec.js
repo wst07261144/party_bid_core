ab963a51354356d86131b7b6d1757c6d188e967c
wst07261144 <wst07261144@gmail.com>
2013-11-23 17:28:55 +0800

    改错误测试

--- data_structure_2/spec/sign_up_spec.js
+++ data_structure_2/spec/sign_up_spec.js
@@ -4,7 +4,7 @@ describe("SignUp", function() {
     beforeEach(function() {
         init_activity_database();
         init_two_activity();
-        localStorage.current_activity_id = "1";
+        localStorage.current_activity = "1";
         localStorage.is_signing_up = "";
     });
 
@@ -17,7 +17,6 @@ describe("SignUp", function() {
         var sms_json = build_sms_json("BM仝键", "13600000000");
         localStorage.is_signing_up = "true";
         notify_sms_received(sms_json);
-
         var activities = JSON.parse(localStorage.activities);
         expect(activities[1].sign_ups.length).toBe(1);
         expect(activities[1].sign_ups[0].name).toBe("仝键");
