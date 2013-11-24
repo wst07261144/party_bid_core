3571103d2bd9c19410b158efa754de0d16d77523
wst07261144 <wst07261144@gmail.com>
2013-11-24 13:34:16 +0800

    创建4个文件并完成创建活动功能

---
+++ data_structure_3/src/Activity.js
@@ -0,0 +1,17 @@
+function Activity(activity_name){
+    this.id=localStorage.activity_id_generator;
+    this.name=activity_name;
+}
+
+Activity.prototype.create=function(){
+    var activities_json=JSON.parse(localStorage.activities)
+    activities_json.push(this)
+    localStorage.activities=JSON.stringify(activities_json)
+    localStorage.current_activity= localStorage.activity_id_generator
+    var id=Number(localStorage.activity_id_generator)+1
+    localStorage.activity_id_generator=id
+}
+
+
+
+
---
+++ data_structure_3/src/Bid.js
@@ -0,0 +1,7 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: wst
+ * Date: 13-11-24
+ * Time: 下午1:13
+ * To change this template use File | Settings | File Templates.
+ */
