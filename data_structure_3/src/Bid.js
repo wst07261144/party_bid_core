0b4e2d96b9327b816f1752ef65daec0398f9ffe4
wst07261144 <wst07261144@gmail.com>
2013-11-24 14:54:44 +0800

    标注当前活动

--- data_structure_3/src/Bid.js
+++ data_structure_3/src/Bid.js
@@ -9,6 +9,7 @@ Bid.create_new_bid=function(current_id){
         return bid.activity_id==current_id
     })
     var name='竞价'+(Number(current_bid.length)+1)
+    localStorage.current_bid=name
     var bids_json=JSON.parse(localStorage.bids)
     bids_json.push(new Bid(name))
     localStorage.bids=JSON.stringify(bids_json)
