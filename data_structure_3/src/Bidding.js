f18305184779a9c4f54471f360774ed4832a2e9b
wst07261144 <wst07261144@gmail.com>
2013-11-24 16:14:30 +0800

    得到竞价结果

--- data_structure_3/src/Bidding.js
+++ data_structure_3/src/Bidding.js
@@ -48,3 +48,48 @@ function process_bidding(bid, phone) {
         }
     }
 }
+Bidding.render_biddings=function(current_activity,current_bid){
+    var unique_bid_array,name ,winner=[]
+    var bid_json=JSON.parse(localStorage.bids)
+    var new_bid_json= _.find(bid_json,function(i_bid){
+        return  i_bid.activity_id==current_activity &&i_bid.name==current_bid
+    })
+    unique_bid_array = get_unique_bid_array(new_bid_json.biddings)
+        if (unique_bid_array != "") {
+        name = find_name(current_activity, unique_bid_array[0].num[0].phone)
+        unique_bid_array[0].num[0]['name'] = name
+        winner.push(unique_bid_array[0].num[0])
+        return winner
+    }
+
+}
+function find_name(current_activity, phone) {
+    var sign_ups_json = JSON.parse(localStorage.sign_ups)
+    var new_sign_ups = _.filter(sign_ups_json, function (sign_up) {
+        return sign_up.activity_id == current_activity
+    })
+    var find_info = _.find(new_sign_ups, function (sign_up) {
+        if (sign_up.phone == phone) {
+            return sign_up
+        }
+    })
+    return find_info.name
+}
+
+function get_unique_bid_array(bid_info) {
+    var person_bid_group_infos = [], bid_result_count = [], get_unique_bid_array = []
+    person_bid_group_infos = _.groupBy(bid_info, function (num) {
+        return Number(num.price);
+    });
+    _.map(person_bid_group_infos, function (value, key) {
+        var temp = person_bid_group_infos[key] = value;
+        bid_result_count.push({"bid": key, "num": temp});
+    })
+    _.map(bid_result_count, function (obj) {
+        if (obj.num.length == 1) {
+            get_unique_bid_array.push(obj);
+        }
+    })
+    return get_unique_bid_array
+}
+
