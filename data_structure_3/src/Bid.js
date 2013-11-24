592bceb654475092c05541022a24c5a83e3b404f
wst07261144 <wst07261144@gmail.com>
2013-11-24 14:40:33 +0800

    创建新竞价

--- data_structure_3/src/Bid.js
+++ data_structure_3/src/Bid.js
@@ -1,7 +1,16 @@
-/**
- * Created with JetBrains RubyMine.
- * User: wst
- * Date: 13-11-24
- * Time: 下午1:13
- * To change this template use File | Settings | File Templates.
- */
+function Bid(name){
+    this.name=name;
+    this.activity_id=localStorage.current_activity
+    this.biddings=[];
+}
+
+Bid.create_new_bid=function(current_id){
+    var current_bid= _.filter(JSON.parse(localStorage.bids),function(bid){
+        return bid.activity_id==current_id
+    })
+    var name='竞价'+(Number(current_bid.length)+1)
+    var bids_json=JSON.parse(localStorage.bids)
+    bids_json.push(new Bid(name))
+    localStorage.bids=JSON.stringify(bids_json)
+    console.log(localStorage.bids)
+}
