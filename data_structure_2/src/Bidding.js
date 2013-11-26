c74120671575e1cbe63e121502754eb8ae2b70f6
wst07261144 <wst07261144@gmail.com>
2013-11-26 15:43:54 +0800

    格式修改

--- data_structure_2/src/Bidding.js
+++ data_structure_2/src/Bidding.js
@@ -2,19 +2,19 @@ function Bid(phone, price) {
     this.phone = phone;
     this.price = price;
 }
-Bid.judge_has_signed = function (bid,phone) {
+Bid.judge_has_signed = function (bid, phone) {
     var signed
     var activities = JSON.parse(localStorage.activities)
     _.each(activities[localStorage.current_activity].sign_ups, function (sign_up) {
         if (Number(sign_up.phone) == Number(phone)) {
             signed = 'true'
-            return  Bid.judge_repeat_bid(bid,phone)
+            return  Bid.judge_repeat_bid(bid, phone)
         }
     })
     return signed
 
 }
-Bid.judge_repeat_bid = function (bid,phone) {
+Bid.judge_repeat_bid = function (bid, phone) {
     var repeat
     var activities = JSON.parse(localStorage.activities)
     _.each(activities[localStorage.current_activity].biddings[localStorage.current_bid]
@@ -23,41 +23,25 @@ Bid.judge_repeat_bid = function (bid,phone) {
                 repeat = 'true'
             }
         })
-    if(repeat != 'true'){
-        return Bid.save_bid(bid,phone)
+    if (repeat != 'true') {
+        return Bid.save_bid(bid, phone)
     }
 }
-Bid.save_bid=function(bid,phone){
+Bid.save_bid = function (bid, phone) {
     var activities = JSON.parse(localStorage.activities)
     var bidder = new Bid(phone, bid)
     activities[localStorage.current_activity].biddings[localStorage.current_bid].push(bidder)
     localStorage.activities = JSON.stringify(activities)
 }
-
-
-function transform_bids_to_view_model(current_activity) {
-    var activities = JSON.parse(localStorage.activities)
-    return activities[current_activity].bids
-}
-
-function transform_biddings_to_view_model(current_activity, current_bid) {
-    var winner = [], unique_bid_array
-    var new_bid_info= Bid.add_name_for_bids(current_activity, current_bid)
-    unique_bid_array = get_unique_bid_array(new_bid_info)
-    if (unique_bid_array != "") {
-        winner.push(unique_bid_array[0].num[0])
-        return winner
-    }
-}
-Bid.add_name_for_bids=function(current_activity, current_bid){
+Bid.add_name_for_bids = function (current_activity, current_bid) {
     var activities = JSON.parse(localStorage.activities)
     var bid_info = activities[current_activity].biddings[current_bid]
-    return _.map(bid_info,function(i_bid){
-       i_bid['name']=Bid.find_name(current_activity,i_bid.phone)
+    return _.map(bid_info, function (i_bid) {
+        i_bid['name'] = Bid.find_name(current_activity, i_bid.phone)
         return i_bid
     })
 }
-Bid.find_name=function(current_activity, phone) {
+Bid.find_name = function (current_activity, phone) {
     var activities = JSON.parse(localStorage.activities)
     var bid_info = activities[current_activity].sign_ups
     var find_info = _.find(bid_info, function (sign_up) {
@@ -67,7 +51,7 @@ Bid.find_name=function(current_activity, phone) {
     })
     return find_info.name
 }
-function get_unique_bid_array(bid_info) {
+Bid.get_unique_bid_array = function (bid_info) {
     var person_bid_group_infos = [], bid_result_count = [], get_unique_bid_array = []
     person_bid_group_infos = _.groupBy(bid_info, function (num) {
         return Number(num.price);
@@ -82,5 +66,19 @@ function get_unique_bid_array(bid_info) {
         }
     })
     return get_unique_bid_array
+}
+
+function transform_bids_to_view_model(current_activity) {
+    var activities = JSON.parse(localStorage.activities)
+    return activities[current_activity].bids
+}
+function transform_biddings_to_view_model(current_activity, current_bid) {
+    var winner = [], unique_bid_array
+    var new_bid_info = Bid.add_name_for_bids(current_activity, current_bid)
+    unique_bid_array = Bid.get_unique_bid_array(new_bid_info)
+    if (unique_bid_array != "") {
+        winner.push(unique_bid_array[0].num[0])
+        return winner
+    }
 }
 
